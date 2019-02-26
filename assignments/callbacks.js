738835e3da139531d50a9ed91613dc0264ef5582
jsmoot93 <47538435+jsmoot93@users.noreply.github.com>
2019-02-26 18:03:02 -0500

    arrayMethods done and callbacks almost done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -58,10 +58,23 @@ multiplyNums(5, 4, function(mult) {
   console.log(mult)
 });
 
+
+const stuff = ['pen', 'notes', 'string', 'mint'];
+
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] = item) {
+      return cb('true');
+    } else {
+      return cb('false');
+    }
+  }
 }
+contains('legoLand', stuff, function(find) {
+  console.log(find)
+});
 
 /* STRETCH PROBLEM */
 
